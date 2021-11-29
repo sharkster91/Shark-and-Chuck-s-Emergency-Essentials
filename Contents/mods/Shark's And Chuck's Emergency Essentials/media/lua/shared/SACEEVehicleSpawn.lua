386fe4e41d80fe3fa4320a17c6ac96ff2842caea
ChuckTheJittai <6071161+ChuckTheSheep@users.noreply.github.com>
2021-11-29 00:18:26 -0500

    Update SACEEVehicleSpawn.lua

--- Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/shared/SACEEVehicleSpawn.lua
+++ Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/shared/SACEEVehicleSpawn.lua
@@ -1,5 +1,3 @@
-
-
 if VehicleZoneDistribution then -- check if the table exists for backwards compatibility
 
 -- ****************************** --
@@ -8,29 +6,29 @@ if VehicleZoneDistribution then -- check if the table exists for backwards compa
 
 -- police
 
-VehicleZoneDistribution.police.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 100};
-VehicleZoneDistribution.police.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 100};
-VehicleZoneDistribution.police.spawnRate = 35;
-VehicleZoneDistribution.police.baseVehicleQuality = 1;
+VehicleZoneDistribution.police.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.police.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.police.spawnRate = 35
+VehicleZoneDistribution.police.baseVehicleQuality = 1
 
 -- fire
-VehicleZoneDistribution.fire.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 100};
-VehicleZoneDistribution.fire.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 100};
-VehicleZoneDistribution.fire.spawnRate = 35;
-VehicleZoneDistribution.fire.baseVehicleQuality = 1;
+VehicleZoneDistribution.fire.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.fire.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.fire.spawnRate = 35
+VehicleZoneDistribution.fire.baseVehicleQuality = 1
 
 -- military
 
 VehicleZoneDistribution.military = VehicleZoneDistribution.military or {}
 VehicleZoneDistribution.military.vehicles = VehicleZoneDistribution.military.vehicles or {}
-VehicleZoneDistribution.military.vehicles["SACEE.VanFEMA"] = {index = -1, spawnChance = 100;
-VehicleZoneDistribution.military.vehicles["SACEE.VanFEMARadio"] = {index = -1, spawnChance = 100};
-VehicleZoneDistribution.military.baseVehicleQuality = 1;
-VehicleZoneDistribution.military.spawnRate = 35;
+VehicleZoneDistribution.military.vehicles["SACEE.VanFEMA"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.military.vehicles["SACEE.VanFEMARadio"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.military.baseVehicleQuality = 1
+VehicleZoneDistribution.military.spawnRate = 35
 
 -- wrecks and pileups 
 
-VehicleZoneDistribution.trafficjamw.vehicles["SACEE.VanStateEmergencyBodies"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.trafficjamw.vehicles["SACEE.PickUpVanStateEmergencyBodies"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.trafficjamw.vehicles["SACEE.VanStateEmergencyBodies"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.trafficjamw.vehicles["SACEE.PickUpVanStateEmergencyBodies"] = {index = -1, spawnChance = 1}
 
 end
