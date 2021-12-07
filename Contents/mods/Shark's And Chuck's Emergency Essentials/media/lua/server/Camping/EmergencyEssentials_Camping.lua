99e98aeb2ccbbec24f8ab38a4b3167d38b845da8
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-07 05:51:21 -0500

    Added new flyers

---
+++ Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/server/Camping/EmergencyEssentials_Camping.lua
@@ -0,0 +1,6 @@
+-- The value is the number of hours of fuel added to a fire.
+campingFuelType.PublicAwarenessFlyer = 5/60.0
+campingFuelType.DisasterAssistanceFlyer = 5/60.0
+-- Types of items that can be used with a lighter/matches to start a fire.
+campingFuelType.PublicAwarenessFlyer = 5/60.0
+campingFuelType.DisasterAssistanceFlyer = 5/60.0
