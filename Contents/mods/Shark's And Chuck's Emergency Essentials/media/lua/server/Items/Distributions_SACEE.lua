92cf41dfcdfa368f876f0e753e5a80e0d329f2bd
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-07 05:52:57 -0500

    Update Distributions_SACEE.lua

--- Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/server/Items/Distributions_SACEE.lua
+++ Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/server/Items/Distributions_SACEE.lua
@@ -2,3 +2,6 @@ require 'Items/ProceduralDistributions'
 
 table.insert(ProceduralDistributions.list.MedicalClinicOutfit.items, "SACEE.BodyBagBox_SACEE");
 table.insert(ProceduralDistributions.list.MedicalClinicOutfit.items, 3);
+
+table.insert(ProceduralDistributions.list.FireStorageTools.items, "SACEE.BodyBagBox_SACEE");
+table.insert(ProceduralDistributions.list.FireStorageTools.items, 3);
