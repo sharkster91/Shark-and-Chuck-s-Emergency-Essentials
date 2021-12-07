b5dd007cda9055c6fc2514b0f809c9e36801cb3e
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-07 05:53:08 -0500

    Update SACEEVehicleSpawn.lua

--- Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/shared/SACEEVehicleSpawn.lua
+++ Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/shared/SACEEVehicleSpawn.lua
@@ -6,14 +6,13 @@ if VehicleZoneDistribution then -- check if the table exists for backwards compa
 
 -- police
 
-VehicleZoneDistribution.police.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 100}
-VehicleZoneDistribution.police.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 100}
-VehicleZoneDistribution.police.spawnRate = 35
+VehicleZoneDistribution.police.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 50}
+VehicleZoneDistribution.police.spawnRate = 60
 VehicleZoneDistribution.police.baseVehicleQuality = 1
 
 -- fire
-VehicleZoneDistribution.fire.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 100}
-VehicleZoneDistribution.fire.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.fire.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 50}
+VehicleZoneDistribution.fire.vehicles["SACEE.PickUpVanStateEmergency"] = {index = -1, spawnChance = 50}
 VehicleZoneDistribution.fire.spawnRate = 35
 VehicleZoneDistribution.fire.baseVehicleQuality = 1
 
@@ -21,14 +20,14 @@ VehicleZoneDistribution.fire.baseVehicleQuality = 1
 
 VehicleZoneDistribution.military = VehicleZoneDistribution.military or {}
 VehicleZoneDistribution.military.vehicles = VehicleZoneDistribution.military.vehicles or {}
-VehicleZoneDistribution.military.vehicles["SACEE.VanFEMA"] = {index = -1, spawnChance = 100}
-VehicleZoneDistribution.military.vehicles["SACEE.VanFEMARadio"] = {index = -1, spawnChance = 100}
+VehicleZoneDistribution.military.vehicles["SACEE.VanFEMA"] = {index = -1, spawnChance = 50}
+VehicleZoneDistribution.military.vehicles["SACEE.VanFEMARadio"] = {index = -1, spawnChance = 50}
 VehicleZoneDistribution.military.baseVehicleQuality = 1
 VehicleZoneDistribution.military.spawnRate = 35
 
 -- wrecks and pileups 
 
-VehicleZoneDistribution.trafficjamw.vehicles["SACEE.VanStateEmergencyBodies"] = {index = -1, spawnChance = 1}
-VehicleZoneDistribution.trafficjamw.vehicles["SACEE.PickUpVanStateEmergencyBodies"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.trafficjamw.vehicles["SACEE.VanStateEmergencyBodies"] = {index = -1, spawnChance = 50}
+VehicleZoneDistribution.trafficjamw.vehicles["SACEE.PickUpVanStateEmergencyBodies"] = {index = -1, spawnChance = 50}
 
 end
