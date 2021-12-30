b3f4db6870eb4ffa7ff5ac979f4185bebc5753d8
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2021-12-29 21:21:36 -0500

    Update SACEEVehicleSpawn.lua

--- Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/shared/SACEEVehicleSpawn.lua
+++ Contents/mods/Shark's And Chuck's Emergency Essentials/media/lua/shared/SACEEVehicleSpawn.lua
@@ -7,12 +7,12 @@ if VehicleZoneDistribution then -- check if the table exists for backwards compa
 -- police
 
 VehicleZoneDistribution.police.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 50}
-VehicleZoneDistribution.police.spawnRate = 60
+VehicleZoneDistribution.police.spawnRate = 45
 VehicleZoneDistribution.police.baseVehicleQuality = 1
 
 -- fire
 VehicleZoneDistribution.fire.vehicles["SACEE.VanStateEmergency"] = {index = -1, spawnChance = 50}
-VehicleZoneDistribution.fire.spawnRate = 35
+VehicleZoneDistribution.fire.spawnRate = 45
 VehicleZoneDistribution.fire.baseVehicleQuality = 1
 
 -- military
@@ -23,6 +23,7 @@ VehicleZoneDistribution.military.vehicles["SACEE.VanFEMA"] = {index = -1, spawnC
 VehicleZoneDistribution.military.vehicles["SACEE.VanFEMARadio"] = {index = -1, spawnChance = 50}
 VehicleZoneDistribution.military.spawnRate = 35
 VehicleZoneDistribution.military.baseVehicleQuality = 1
+
 -- wrecks and pileups 
 
 VehicleZoneDistribution.trafficjamw.vehicles["SACEE.VanStateEmergencyBodies"] = {index = -1, spawnChance = 3}
